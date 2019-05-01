2b94573e8f896521d09c34468ba54803edcc4dc1
Michael Redig <mredig@gmail.com>
2019-05-01 09:39:59 -0500

    initial commit

---
+++ QuickOrderedSet/QuickOrderedSet.h
@@ -0,0 +1,19 @@
+//
+//  QuickOrderedSet.h
+//  QuickOrderedSet
+//
+//  Created by Michael Redig on 5/1/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuickOrderedSet.
+FOUNDATION_EXPORT double QuickOrderedSetVersionNumber;
+
+//! Project version string for QuickOrderedSet.
+FOUNDATION_EXPORT const unsigned char QuickOrderedSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickOrderedSet/PublicHeader.h>
+
+
