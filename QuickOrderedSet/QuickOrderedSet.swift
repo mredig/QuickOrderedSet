bde9337974c8f3d3fe07b5f4394e7781d039edec
Michael Redig <mredig@gmail.com>
2019-06-02 01:18:34 -0500

    radical overhaul
    changed contents to just be a set instead of a dictionary - this simplifies things and just makes code a lot easier to manage
    	the downside is that retrieving the index of an element is no longer 0(1)
    	it might be mitigated (arguable whether worth it or not) by creating a wrapper struct/class for the contents that tracks index and holds the original value as well
    modified behavior of appending/inserting objects
    	now it silently does nothing if an object already exists when inserting (good thing this is a small project and probably no one is using it!)
    updated tests to account for new behavior
    added comment documentation

--- QuickOrderedSet/QuickOrderedSet.swift
+++ QuickOrderedSet/QuickOrderedSet.swift
@@ -11,74 +11,84 @@ import Foundation
 
 public struct QuickOrderedSet<Type: Codable & Hashable> {
 	private(set) var sequencedContents: ContiguousArray<Type>
-	private(set) var contents: [Type: Int]
+	private(set) var contents: Set<Type>
 
 	init() {
 		sequencedContents = []
-		contents = [:]
+		contents = Set<Type>()
 	}
 
+	/// Returns the index of the element, if it's included in the ordered set. Nil otherwise.
 	func index(of object: Type) -> Int? {
-		if let index = contents[object] {
+		if let index = sequencedContents.firstIndex(of: object) {
 			return index
 		}
 		return nil
 	}
 
+	/// Appends a new element *if it doesn't already exist in the ordered set*
 	mutating func append(_ element: Type) {
-		if contents[element] != nil {
-			remove(element)
+		if !contents.contains(element) {
+			contents.insert(element)
+			sequencedContents.append(element)
 		}
-		contents[element] = contents.count
-		sequencedContents.append(element)
 	}
 
+	/// Removes an element if it's present in the ordered set
 	mutating func remove(_ element: Type) {
-		guard let index = contents[element] else { return }
+		guard contents.contains(element),
+			let index = sequencedContents.firstIndex(of: element) else { return }
 		remove(at: index)
 	}
 
+	/// Removes the element at the requested index. Crashes if index is out of bounds, however.
 	mutating func remove(at index: Int) {
-//		guard sequencedContents.count > index else { return } // dno if this is a good idea or not
+		precondition(sequencedContents.count > index, "Index '\(index)' out of bounds")
 		let objectAtIndex = sequencedContents[index]
-		contents.removeValue(forKey: objectAtIndex)
+		contents.remove(objectAtIndex)
 		sequencedContents.remove(at: index)
-		for index in index..<sequencedContents.count {
-			contents[sequencedContents[index]] = index
-		}
 	}
 
+	/// Returns a Bool determining if an element is present in the ordered set
 	func contains(_ element: Type) -> Bool {
-		return contents[element] != nil
+		return contents.contains(element)
 	}
 
+	/**
+	Counters typical Ordered Set behavior:
+		* if the value already exists in the array, it does nothing...
+			* (which is normal behavior)
+		* BUT if the value already exists and is at the specified index, it replaces the existing value with the new one.
+			* This is counter the normal behavior.
+			* this is done for my own personal project where I'm using a counter object that consists of a value and a counter;
+				the counter is not evaluated in the hash, but when incrementing the counter, it'll need to replace the old value.
+	*/
 	public subscript(index: Int) -> Type {
 		get {
 			return sequencedContents[index]
 		}
 		set {
-			var index = index
-			var removedElement = false
-			if let oldIndex = contents[newValue], oldIndex != index {
-				//remove existing value (coalesce to the rest of the stuff)
-				remove(newValue)
-				removedElement = true
-				if oldIndex < index {
-					index -= 1
+			if contents.contains(newValue) {
+				if sequencedContents[index] == newValue {
+					sequencedContents[index] = newValue
+					contents.remove(sequencedContents[index])
+					contents.insert(newValue)
 				}
+			} else {
+				let oldValue = sequencedContents[index]
+				sequencedContents[index] = newValue
+				contents.insert(newValue)
+				contents.remove(oldValue)
 			}
-			if removedElement && index == sequencedContents.count {
-				append(newValue)
-			}
-			if sequencedContents[index] != newValue {
-				contents.removeValue(forKey: sequencedContents[index])
-			}
-			sequencedContents[index] = newValue
-			contents[newValue] = index
 		}
 	}
 
 	//TODO: Insert at
+	//TODO: replace at
+	//TODO: replace element
+	//TODO: set at
+	//TODO: move to index
+	//TODO: exchange at index with index
 	public var count: Int {
 		return sequencedContents.count
 	}
@@ -115,10 +125,8 @@ extension QuickOrderedSet: Codable {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
 		let tempContents = try container.decode([Type].self, forKey: .sequencedContents)
 		sequencedContents = ContiguousArray(tempContents)
-		contents = [:]
-		for (index, element) in sequencedContents.enumerated() {
-			contents[element] = index
-		}
+		contents = Set(sequencedContents)
+		precondition(contents.count == sequencedContents.count, "Decoded value not valid set")
 	}
 }
 
@@ -126,5 +134,4 @@ extension QuickOrderedSet: CustomStringConvertible {
 	public var description: String {
 		return sequencedContents.description
 	}
-
 }
--- QuickOrderedSetTests/QuickOrderedSetTests.swift
+++ QuickOrderedSetTests/QuickOrderedSetTests.swift
@@ -21,169 +21,140 @@ class QuickOrderedSetTests: XCTestCase {
 
 	func defaultValues() -> (set: QuickOrderedSet<Int>, array: [Int]) {
 		var testingOrderedSet = QuickOrderedSet<Int>()
-		var sideArray = [Int]()
+		var truthArray = [Int]()
 
 		for index in 1...10 {
 			testingOrderedSet.append(index)
-			sideArray.append(index)
+			truthArray.append(index)
 		}
-		return (testingOrderedSet, sideArray)
+		return (testingOrderedSet, truthArray)
 	}
 
 	func testAppendNew() {
-		var (testingOrderedSet, sideArray) = defaultValues()
+		var (testingOrderedSet, truthArray) = defaultValues()
 
 		testingOrderedSet.append(11)
-		sideArray.append(11)
 		XCTAssert(testingOrderedSet.contents.count == testingOrderedSet.sequencedContents.count)
 
-		//should be run after every edit
-		for (index, element) in testingOrderedSet.sequencedContents.enumerated() {
-			XCTAssert(testingOrderedSet.contents[element] == index, "\(testingOrderedSet.contents)")
-		}
+		truthArray.append(11)
+		XCTAssert(Array(testingOrderedSet.sequencedContents) == truthArray)
 
+		//should be run after every edit
+		XCTAssert(testingOrderedSet.sequencedContents.count == testingOrderedSet.contents.count)
+		for element in testingOrderedSet.sequencedContents {
+			XCTAssert(testingOrderedSet.contents.contains(element), "\(testingOrderedSet.contents)")
+		}
 	}
 
 	func testAppendExisting() {
-		var (testingOrderedSet, sideArray) = defaultValues()
+		var (testingOrderedSet, truthArray) = defaultValues()
 
-		// append existing
+		// append existing (should be no change)
 		testingOrderedSet.append(1)
-		sideArray.append(1)
-		sideArray.removeFirst()
 		XCTAssert(testingOrderedSet.contents.count == testingOrderedSet.sequencedContents.count)
-		XCTAssert(sideArray.count == testingOrderedSet.sequencedContents.count)
-		for index in sideArray.indices {
-			XCTAssert(sideArray[index] == testingOrderedSet[index], "\(sideArray) != \(testingOrderedSet)")
-		}
+
+		XCTAssert(Array(testingOrderedSet.sequencedContents) == truthArray)
 
 		//should be run after every edit
-		for (index, element) in testingOrderedSet.sequencedContents.enumerated() {
-			XCTAssert(testingOrderedSet.contents[element] == index, "\(testingOrderedSet.contents)")
+		XCTAssert(testingOrderedSet.sequencedContents.count == testingOrderedSet.contents.count)
+		for element in testingOrderedSet.sequencedContents {
+			XCTAssert(testingOrderedSet.contents.contains(element), "\(testingOrderedSet.contents)")
 		}
-
 	}
 
-	func testSetExistingMoveDown() {
-		var (testingOrderedSet, sideArray) = defaultValues()
+	func testSetExisting() {
+		var (testingOrderedSet, truthArray) = defaultValues()
 
-		// set existing, moving down
+		// set existing (should be no change)
 		testingOrderedSet[5] = 10
-		sideArray.remove(at: 9)
-		sideArray.remove(at: 5)
-		sideArray.insert(10, at: 5)
 		XCTAssert(testingOrderedSet.contents.count == testingOrderedSet.sequencedContents.count)
-		XCTAssert(sideArray.count == testingOrderedSet.sequencedContents.count)
-		for index in sideArray.indices {
-			XCTAssert(sideArray[index] == testingOrderedSet[index], "\(sideArray) != \(testingOrderedSet)")
-		}
+
+		XCTAssert(Array(testingOrderedSet.sequencedContents) == truthArray)
 
 		//should be run after every edit
-		for (index, element) in testingOrderedSet.sequencedContents.enumerated() {
-			XCTAssert(testingOrderedSet.contents[element] == index, "\(testingOrderedSet.contents)")
-		}
-	}
-
-	func testSetExistingMoveUp() {
-		var (testingOrderedSet, sideArray) = defaultValues()
-
-		// set existing, moving up
-		testingOrderedSet[4] = 1
-		sideArray.remove(at: 4)
-		sideArray.insert(1, at: 4)
-		sideArray.remove(at: 0)
-		XCTAssert(testingOrderedSet.contents.count == testingOrderedSet.sequencedContents.count,
-				  "\(testingOrderedSet.contents.count) and \( testingOrderedSet.sequencedContents.count)")
-		XCTAssert(sideArray.count == testingOrderedSet.sequencedContents.count)
-		for index in sideArray.indices {
-			XCTAssert(sideArray[index] == testingOrderedSet[index], "\(sideArray) != \(testingOrderedSet)")
-		}
-
-		//should be run after every edit
-		for (index, element) in testingOrderedSet.sequencedContents.enumerated() {
-			XCTAssert(testingOrderedSet.contents[element] == index, "\(testingOrderedSet.contents)")
+		XCTAssert(testingOrderedSet.sequencedContents.count == testingOrderedSet.contents.count)
+		for element in testingOrderedSet.sequencedContents {
+			XCTAssert(testingOrderedSet.contents.contains(element), "\(testingOrderedSet.contents)")
 		}
 	}
 
 	func testSetNew() {
-		var (testingOrderedSet, sideArray) = defaultValues()
+		var (testingOrderedSet, truthArray) = defaultValues()
 
 		//set new
 		testingOrderedSet[4] = 15
-		sideArray[4] = 15
 		XCTAssert(testingOrderedSet.contents.count == testingOrderedSet.sequencedContents.count)
-		XCTAssert(sideArray.count == testingOrderedSet.sequencedContents.count)
-		for index in sideArray.indices {
-			XCTAssert(sideArray[index] == testingOrderedSet[index], "\(sideArray) != \(testingOrderedSet)")
-		}
+
+		truthArray[4] = 15
+		XCTAssert(Array(testingOrderedSet.sequencedContents) == truthArray)
 
 		//should be run after every edit
-		for (index, element) in testingOrderedSet.sequencedContents.enumerated() {
-			XCTAssert(testingOrderedSet.contents[element] == index, "\(testingOrderedSet.contents)")
+		XCTAssert(testingOrderedSet.sequencedContents.count == testingOrderedSet.contents.count)
+		for element in testingOrderedSet.sequencedContents {
+			XCTAssert(testingOrderedSet.contents.contains(element), "\(testingOrderedSet.contents)")
 		}
 	}
 
 	func testSetReplaceWithSame() {
-		var (testingOrderedSet, sideArray) = defaultValues()
-		//		var (originalSet, _) = defaultValues()
+		var (testingOrderedSet, truthArray) = defaultValues()
 
 		testingOrderedSet[0] = 1
 		XCTAssert(testingOrderedSet.contents.count == testingOrderedSet.sequencedContents.count)
-		XCTAssert(sideArray.count == testingOrderedSet.sequencedContents.count)
-		for index in sideArray.indices {
-			XCTAssert(sideArray[index] == testingOrderedSet[index], "\(sideArray) != \(testingOrderedSet)")
-		}
+
+		truthArray[0] = 1
+		XCTAssert(Array(testingOrderedSet.sequencedContents) == truthArray)
 
 		//should be run after every edit
-		for (index, element) in testingOrderedSet.sequencedContents.enumerated() {
-			XCTAssert(testingOrderedSet.contents[element] == index, "\(testingOrderedSet.contents)")
+		XCTAssert(testingOrderedSet.sequencedContents.count == testingOrderedSet.contents.count)
+		for element in testingOrderedSet.sequencedContents {
+			XCTAssert(testingOrderedSet.contents.contains(element), "\(testingOrderedSet.contents)")
 		}
 	}
 
 	func testSetEndValue() {
-		var (testingOrderedSet, sideArray) = defaultValues()
+		var (testingOrderedSet, truthArray) = defaultValues()
 
-		let endValue = sideArray.count - 1
-		testingOrderedSet[endValue] = 15
-		sideArray[endValue] = 15
+		let endIndex = testingOrderedSet.count - 1
+		testingOrderedSet[endIndex] = 15
 		XCTAssert(testingOrderedSet.contents.count == testingOrderedSet.sequencedContents.count)
-		XCTAssert(sideArray.count == testingOrderedSet.sequencedContents.count)
-		for index in sideArray.indices {
-			XCTAssert(sideArray[index] == testingOrderedSet[index], "\(sideArray) != \(testingOrderedSet)")
-		}
+
+		truthArray[endIndex] = 15
+		XCTAssert(Array(testingOrderedSet.sequencedContents) == truthArray)
 
 		//should be run after every edit
-		for (index, element) in testingOrderedSet.sequencedContents.enumerated() {
-			XCTAssert(testingOrderedSet.contents[element] == index, "\(testingOrderedSet.contents)")
+		XCTAssert(testingOrderedSet.sequencedContents.count == testingOrderedSet.contents.count)
+		for element in testingOrderedSet.sequencedContents {
+			XCTAssert(testingOrderedSet.contents.contains(element), "\(testingOrderedSet.contents)")
 		}
 	}
 
 	func testRemoving() {
-		var (testingOrderedSet, sideArray) = defaultValues()
+		var (testingOrderedSet, truthArray) = defaultValues()
 
 		// removing inside
 		testingOrderedSet.remove(at: 8)
-		sideArray.remove(at: 8)
 		XCTAssert(testingOrderedSet.contents.count == testingOrderedSet.sequencedContents.count)
-		XCTAssert(sideArray.count == testingOrderedSet.sequencedContents.count)
-		for index in sideArray.indices {
-			XCTAssert(sideArray[index] == testingOrderedSet[index], "\(sideArray) != \(testingOrderedSet)")
-		}
+
+		truthArray.remove(at: 8)
+		XCTAssert(Array(testingOrderedSet.sequencedContents) == truthArray)
 
 		//should be run after every edit
-		for (index, element) in testingOrderedSet.sequencedContents.enumerated() {
-			XCTAssert(testingOrderedSet.contents[element] == index, "\(testingOrderedSet.contents)")
+		XCTAssert(testingOrderedSet.sequencedContents.count == testingOrderedSet.contents.count)
+		for element in testingOrderedSet.sequencedContents {
+			XCTAssert(testingOrderedSet.contents.contains(element), "\(testingOrderedSet.contents)")
 		}
 	}
 
 	func testBasic() {
-		let (testingOrderedSet, _) = defaultValues()
+		let (testingOrderedSet, truthArray) = defaultValues()
 
 		XCTAssert(testingOrderedSet.contents.count == testingOrderedSet.sequencedContents.count)
+		XCTAssert(Array(testingOrderedSet.sequencedContents) == truthArray)
 
 		//should be run after every edit
-		for (index, element) in testingOrderedSet.sequencedContents.enumerated() {
-			XCTAssert(testingOrderedSet.contents[element] == index, "\(testingOrderedSet.contents)")
+		XCTAssert(testingOrderedSet.sequencedContents.count == testingOrderedSet.contents.count)
+		for element in testingOrderedSet.sequencedContents {
+			XCTAssert(testingOrderedSet.contents.contains(element), "\(testingOrderedSet.contents)")
 		}
 	}
 }
